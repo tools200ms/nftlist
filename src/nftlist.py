fadac31b420c2a70016b75d20b7fcef4e44bb68d
Barnaba Mateusz <barnaba@200ms.net>
2024-03-21 18:02:14 +0100

    Refactoring + line parser under implementation

--- src/nftlist.py
+++ src/nftlist.py
@@ -44,17 +44,33 @@ class BinaryArg:
 
 
 class StrArg:
-    @staticmethod
-    @abstractmethod
-    def validate(arg):
-        pass
+    def __init__(self, arg, sarg):
+        self._arg = arg
+        self._sarg = sarg
+        self._assocobj = None
 
+    def getArg(self) -> str: return self._arg
+
+    def getSArg(self) -> str: return self._sarg
+
+    def validate(self, arg):
+        if self._assocobj != None:
+            raise ConfError('Illegal argument re-declaration: ' + self._arg)
+
+        return arg == self._arg or arg == self._sarg
+
+    def getAssocObj(self):
+        if self._assocobj == None:
+            raise Exception('Missing argument(s) for: ' + self._arg)
+
+        return self._assocobj
     @abstractmethod
     def feed(self):
         pass
 
 class StrArgWCnt(StrArg):
-    def __init__(self):
+    def __init__(self, arg, sarg):
+        super().__init__(arg, sarg)
         self.__arg_feed_cnt = -1
 
     def postincr(self, skip = False):
@@ -70,6 +86,76 @@ class ConfError(Exception):
 class DataError(Exception):
     pass
 
+
+class InclDirPath:
+    __DEFAULT = '/etc/nftlists/included'
+
+    def __init__(self, path = __DEFAULT):
+        self.__path = normpath(path)
+        self.__verified = False
+
+    def verifyPath(self):
+        if self.__verified:
+            return
+
+        if not isdir(self.__path) and exists(self.__path):
+            raise ConfError('--includedir should be a directory path')
+        else:
+            raise ConfError('--includedir does not point to existing directory')
+
+        # rewrite to realpath
+        self.__path = realpath(self.__path)
+        self.__verified = True
+
+    def getFilePath(self, file_name):
+        self.verifyPath()
+        return os.path.join(self.__path, file_name)
+
+class NFTListDirective:
+    pass
+
+class SetDirective(NFTListDirective):
+    def __init__(self, family, tablen, setn):
+        errors = []
+
+        match family:
+            case 'ip' | 'ip6' | 'inet' | 'arp' | 'bridge' | 'netdev':
+                self.__family = family
+            case '-':
+                self.__family = None
+            case _:
+                errors.append('Incorrect family name or no \'-\' qualifier provided')
+
+        if re.fullmatch('([a-zA-Z0-9_\.\-]){1,64}', tablen) != None:
+            self.__tablen = tablen
+        elif tablen == '-':
+            self.__tablen = None
+        else:
+            errors.append('Illegal table name or no \'-\' qualifier provided')
+
+        if re.fullmatch('([a-zA-Z0-9_\.\-]){1,64}', setn) != None:
+            self.__setn = setn
+        elif setn == '-':
+            self.__setn = None
+        else:
+            errors.append('Illegal set name or no \'-\' qualifier provided')
+
+        if len(errors) != 0:
+            errors.insert(0, 'Cannot load set, following errors has been encounted: ')
+            raise ConfError('\n * '.join(errors))
+    def isCompleate(self):
+        pass
+
+    def merge(self, perv_set):
+        pass
+
+class InclDirective(NFTListDirective):
+    def __init__(self, file_name):
+        self.__file_name = normpath(file_name)
+
+class OnPanicDirective(NFTListDirective):
+    def __init__(self):
+        pass
 
 class Program:
     @staticmethod
@@ -107,26 +193,20 @@ class Program:
             return res
 
     class ConfPath(CmdOption):
-        @staticmethod
-        def DEFAULT() -> str: return '/etc/nftlists/enabled'
+        __DEFAULT = '/etc/nftlists/enabled'
 
         def getPath(self):
             return self.__path
 
-        def __init__(self, path : str = None):
-            if path == None:
-                self.__path = Program.ConfPath.DEFAULT()
-            else:
-                self.__path = path
-
-            if isdir(self.__path):
+        def __init__(self, path : str = __DEFAULT):
+            if isdir(path):
                 self.__isdir = True
-            elif isfile(self.__path):
+            elif isfile(path):
                 self.__isdir = False
             else:
                 raise Exception('Configuration file or direcotry not found')
 
-            self.__path = realpath(self.__path)
+            self.__path = realpath(path)
         def isDir(self):
             return self.__isdir
         @staticmethod
@@ -135,16 +215,9 @@ class Program:
 
     class SetArg(StrArgWCnt):
         def __init__(self):
-            super().__init__()
+            super().__init__('--set', '-s')
             self._family = None
             self._table = None
-            self._set = None
-
-        @staticmethod
-        def validate(arg):
-            if arg == '--set' or arg == '-s':
-                return Program.SetArg()
-            return None
 
         def feed(self, arg):
             if arg.startswith('--'):
@@ -156,58 +229,31 @@ class Program:
                 case 1:
                     self._table = arg
                 case 2:
-                    self._set = arg
                     # end of data list
+                    self._assocobj = SetDirective(self._family, self._table, arg)
                     return None
                 case _:
                     raise Exception('Something wrong went with a code flow')
 
             return self
 
-    class SetDirect(SetArg):
-        def validate(self, arg):
-            if arg == '@set':
-                return Program.SetDirect()
-            return None
-
     class InclDirArg(StrArg):
-        DEFAULT = '/etc/nftlists/included'
-
         def __init__(self, path : str = None):
-            if path == None:
-                path = Program.InclDirArg.DEFAULT
-
-            self.setPath(path)
-        def setPath(self, path : str):
-            if isdir(arg):
-                self.__path = path
-            # specifig error msg.
-            elif exists(path):
-                raise ConfError('--includedir should be a directory path')
-            else:
-                raise ConfError('--includedir does not point to existing directory')
-
-            self.__path = realpath(self.__path)
-
-        def getPath(self) -> str:
-            return self.__path
-        def validate(arg):
-            if arg == '--includedir' or arg == '-D':
-                return Program.InclDirArg(None)
-            return None
+            super().__init__('--includedir', '-D')
 
         def feed(self, arg):
-            self.setPath(arg)
+            self._assocobj = InclDirPath(arg)
+            self._assocobj.verifyPath()
             return None
 
     class HelpArg (BinaryArg):
 
         @staticmethod
         def validate(arg):
-            return arg == '--help' or '-h'
+            return arg == '--help' or arg == '-h'
 
         @staticmethod
-        def print(self):
+        def print():
             print(f"Command syntax: \
 {Program.command()} <update|purge|panic> [conf. path] [--set <address family> <table> <set>] [--includedir <path>]\
 \
@@ -235,24 +281,61 @@ $(basename $0) --version | -v\
     class VersionArg(BinaryArg):
         @staticmethod
         def validate(arg):
-            return arg == '--version' or '-v'
+            return arg == '--version' or arg == '-v'
 
         @staticmethod
         def print():
             print(f"NFT List, version: {Program.Version()}\
 Created by {Program.License()}, released with {Program.License()}")
 
+def parse_line(line: str) -> None:
+    line = line.strip()
+
+    if not line:
+        return
+
+    # index of a comment mark '#'
+    c_idx = line.find('#')
+    if c_idx != -1:
+        # cut comments
+        line = line[0:c_idx].strip()
+        if not line:
+            return
+
+    if line[0] == '@':
+        # directive has been encounted
+        direct = line.split()
+        match direct[0]:
+            case '@set':
+                if len(direct) != 4:
+                    raise ConfError('@set directive requiers 4 arguments')
+                return SetDirective(direct[1], direct[2], direct[3])
+
+            case '@include':
+                if len(direct) != 2:
+                    raise ConfError('@ include directive requiers 1 argument')
+                return InclDirective(direct[1])
+            case '@query':
+                raise Exception('Not implemented')
+            case '@onpanic':
+                if len(direct) != 2:
+                    raise ConfError('@onpanic directive requiers 1 argument')
+                return OnPanicDirective(direct[1])
+
+    # clasify to what data set resource belongs to
+    
+
 action = None
 config = None
 
-incl_arg = None
-set_arg = None
+set_arg = Program.SetArg()
+incl_arg = Program.InclDirArg()
 
-feed = None
+in_feed = None
 
 for arg in sys.argv[1:]:
-    if feed != None:
-        feed = feed.feed(arg)
+    if in_feed != None:
+        in_feed = in_feed.feed(arg)
         continue
 
     if arg.startswith('-'):
@@ -263,20 +346,12 @@ for arg in sys.argv[1:]:
             Program.VersionArg.print()
             exit(0)
         else:
-            feed = Program.SetArg.validate(arg)
-
-            if feed != None:
-                if set != None:
-                    raise ConfError('Illegal argument re-declaration: ' + arg)
-                set = feed
+            if set_arg.validate(arg):
+                in_feed = set_arg
                 continue
 
-            feed = Program.InclDirArg.validate(arg)
-            if feed != None:
-                if incl_arg != None:
-                    raise ConfError('Illegal argument re-declaration: ' + arg)
-
-                incl_arg = feed
+            if incl_arg.validate(arg):
+                in_feed = incl_arg
                 continue
 
             raise ConfError('Unsupported argument: ' + arg)
@@ -292,6 +367,9 @@ for arg in sys.argv[1:]:
                 continue
 
         raise ConfError('Unidentified operation: ' + arg)
+
+# if feed != None:
+
 
 # Get default values if not passed with command arguments
 if action == None:
@@ -318,12 +396,31 @@ else:
 
 # ensure files are in alpha-numeric order:
 conf_files.sort()
+LINE_LEN_LIMIT = 2048
+LINE_CNT_LIMIT = 1024*1024
 
+set_direct = set_arg.getAssocObj()
 # parse files
-
 for cf in conf_files:
     cf_path = join(conf_path, cf)
     print('Reading \'' + cf_path + '\'')
+
+    try:
+        with open(cf_path, 'r') as cf:
+            line_no = 0
+            while line_no < LINE_CNT_LIMIT:
+                line_no += 1
+                line = cf.readline(LINE_LEN_LIMIT)
+                if not line:
+                    break
+                parse_line(line)
+
+            if line_no == LINE_CNT_LIMIT:
+                raise DataError(f"Max line number count been excceded\n MAX_CNT_LIMIT: {LINE_CNT_LIMIT}, file:{cf_path}")
+    except Exception as ex:
+        print(f"Error in file: '{cf_path}' at line: {line_no}\n" + ' * \n'.join(ex.args))
+
+
 
 if set_arg == None:
     print('No set has been encouned, exiting')
